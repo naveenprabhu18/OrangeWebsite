ec24355119a13c29238c55ee3e8b0f6b26586686
naveenprabhu18 <naveenviki13@gmail.com>
2024-12-13 10:24:04 +0530

    First Time Commit

---
+++ OrangeWebsite/src/test/java/POMClass/POMClass.java
@@ -0,0 +1,10 @@
+package POMClass;
+
+public class POMClass {
+	public void addName(String name) {
+		//userName.sendKeys(name);
+	}
+	public void addPassWord(String password) {
+		//Password.sendKeys(password);
+	}
+}
