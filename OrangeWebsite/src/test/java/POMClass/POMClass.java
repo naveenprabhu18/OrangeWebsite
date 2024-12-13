b9fb868a2af1b4692e61b2efa781a63fc81db285
naveenprabhu18 <naveenviki13@gmail.com>
2024-12-13 13:18:36 +0530

    Added a class POM

--- OrangeWebsite/src/test/java/POMClass/POMClass.java
+++ OrangeWebsite/src/test/java/POMClass/POMClass.java
@@ -1,11 +1,36 @@
 package POMClass;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class POMClass {
-	public void addName(String name) {
-		//userName.sendKeys(name);
-	}
-	public void addPassWord(String password) {
-		//Password.sendKeys(password);
-		System.out.println("byee");
-	}
+	
+	POMClass(WebDriver driver){
+		PageFactory.initElements(driver, this);
+		}
+	
+	@FindBy(xpath="//*[@name='FullName']")
+	WebElement UserName;
+	@FindBy (xpath="//*[@name='Email']")WebElement Email;
+	@FindBy (xpath="//*[@name='CompanyName']")WebElement CompanyName;
+	@FindBy (xpath="//*[@name='Contact']")WebElement Contact;
+	@FindBy (xpath="//*[@name='action_submitForm']")WebElement ONCLICK;
+void EnterUserName(String name ) {
+UserName.sendKeys(name);
+}
+void EnterEmail(String email) {
+	Email.sendKeys(email);
+}
+void EnterCompanyName(String companyname) {
+	CompanyName.sendKeys(companyname);
+	
+}
+void EnterContact(String contact) {
+	Contact.sendKeys(contact);
+}
+void Submit() {
+	ONCLICK.click();
+}
 }
